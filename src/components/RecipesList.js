6ac163ee36efefdded57ea459eaf6574c0552133
Sayyed-Abrar-Akhtar <akhtars10@uni.coventry.ac.uk>
2021-05-09 20:03:41 +0545

    Set up headless cms and fetch data using plugin

---
+++ src/components/RecipesList.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const RecipesList = ({ recipes }) => {
+  return (
+    <div>
+      <h4>This is recipes list</h4>
+    </div>
+  )
+}
+
+export default RecipesList
--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,11 +1,14 @@
 import React from "react"
+import AllRecipes from "../components/AllRecipes"
 
 import Layout from "../components/Layout"
 
 const Recipes = () => {
   return (
     <Layout>
-      <h1>Recipe</h1>
+      <main className="page">
+        <AllRecipes />
+      </main>
     </Layout>
   )
 }
